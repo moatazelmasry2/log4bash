768f72e06826302e0b9f37b07b50de2c8c7ecc35
Moataz Elmasry <moataz.elmasry2@gmail.com>
2014-09-01 00:17:16 +0200

    Added colors to script

---
+++ test.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+source ./scripts/log4bash
+log_trace "this is a trace"
+log_debug "this is a debug"
+log_info "this is a info"
+log_warning "this is a warning"
+log_error "this is a error"
+log_fatal "this is a fatal"
+
